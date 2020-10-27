518132ed354aa84a7f4f1accc3677861cc022484
murokisr <murokisr@gmail.com>
2020-10-27 18:31:32 -0500

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <div>Add Appointments</div>
+                <div>Search Appointments</div>
+                <div>List Appointments</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
