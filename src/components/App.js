d63999bf97151065310bb3f61d0d5f1340616129
murokisr <murokisr@gmail.com>
2020-10-28 18:51:06 -0500

    updated file AddAppointments.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,68 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 import '../css/App.css';
 
+import AddAppointments from './AddAppointments';
+import SearchAppointments from './SearchAppointments';
+import ListAppointments from './ListAppointments';
+import {without} from 'lodash';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myAppointments:[],
+      formDisplay: false,
+      lastIndex: 0
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+  }
+
+  toggleForm() {
+    this.setState({
+      formDisplay: !this.state.formDisplay
+    });
+  }
+
+  addAppointment(apt){
+    let tempApts = this.state.myAppointments;
+    apt.aptId = this.state.lastIndex;
+    //to push element into array of data//
+    tempApts.unshift(apt);
+    this.setState({
+      myAppointments: tempApts,
+      lastIndex: this.state.lastIndex +1 
+    });
+  }
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    })
+  }
+
+  ///bring in data using lifecycle method//
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({lastIndex:this.state.lastIndex + 1 });
+          return item;
+        });
+        //push the above variable 'apts' to state as below
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+
   render() {
     return (
       <main className="page bg-white" id="petratings">
@@ -10,9 +71,14 @@ class App extends Component {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <div>Add Appointments</div>
-                <div>Search Appointments</div>
-                <div>List Appointments</div>
+                < AddAppointments 
+                    formDisplay={this.state.formDisplay}
+                    toggleForm={this.toggleForm}
+                    addAppointment = {this.addAppointment}
+                />
+                < SearchAppointments />
+                < ListAppointments appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment} />
               </div>
             </div>
           </div>
